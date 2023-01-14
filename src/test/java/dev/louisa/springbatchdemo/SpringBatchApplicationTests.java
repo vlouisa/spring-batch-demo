6df456810a83e2f15eb47122f07a1e98eae64266
Victor Louisa <victor.louisa@gmail.com>
2023-01-14 16:41:24 +0100

    Initial commit

---
+++ src/test/java/dev/louisa/springbatchdemo/SpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.louisa.springbatchdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
